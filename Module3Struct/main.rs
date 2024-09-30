42141a2cb40999731f7e24bf0e4d32896d58c79b
Humanie <160002624+Humaniey@users.noreply.github.com>
2024-09-30 21:18:56 +0000

    Completed module 3 structure assignment

--- Module3Struct/main.rs
+++ Module3Struct/main.rs
@@ -1,24 +1,62 @@
-use std::io::{self, Read, Write};
+use std::io::{self, BufReader, BufRead, Write};
+use std::fs::File;
 
-struct Person {
-    name: String,
-    age: u32,
+struct Car {
+    make: String,
+    model: String,
+    year: u32
 }
 
-fn reading_from_console() {
+fn main() {
     let mut buffer = String::new();
 
     print!("What's your car's make? ");
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut buffer).unwrap();
-    let name = buffer.trim().to_string();
+    let make = buffer.trim().to_string();
     buffer.clear();
 
-    // print!("How old are you? ");
-    // io::stdout().flush().unwrap();
-    // io::stdin().read_line(&mut buffer).unwrap();
-    // let age = buffer.trim().parse().unwrap();
+    print!("What's your {}'s model? ", make);
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let model = buffer.trim().to_string();
+    buffer.clear();
 
-    let person = Person { name, age };
-    println!("Hi {}, you are {} years old!", person.name, person.age);
+    print!("What year is your {} {}? ", make, model);
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let year = buffer.trim().parse().unwrap();
+    buffer.clear();
+
+    let car = Car { make, model, year };
+
+    let mut file = File::create("user_info.txt").unwrap();
+    writeln!(file, "{}\n{}\n{}", car.make, car.model, car.year).unwrap();
+
+    let file = File::open("user_info.txt").unwrap();
+    let mut reader = BufReader::new(file);
+
+    reader.read_line(&mut buffer).unwrap();
+    // buffer.pop();    #Another way to get rid of '\n' in the buffer
+    // let file_make = buffer.clone();  #'.clone() is necessary if not using 'to_string()'
+    let file_make = buffer.trim().to_string();
+    buffer.clear();
+
+    reader.read_line(&mut buffer).unwrap();
+    let file_model = buffer.trim().to_string();
+    buffer.clear();
+
+    reader.read_line(&mut buffer).unwrap();
+    let file_year = buffer.trim().parse().unwrap();
+    buffer.clear();
+    
+    let file_car = Car {
+        make:   file_make,
+        model:  file_model,
+        year:   file_year
+    };
+
+    println!("Your car is a {} {} {}", file_car.year, file_car.make, file_car.model);
+
+    
 }
